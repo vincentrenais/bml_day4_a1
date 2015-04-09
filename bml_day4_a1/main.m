71e88b2315ee53329949e6064d570c6ad365b7d4
Vincent Renais <vincentrenais@gmail.com>
2015-04-09 15:26:02 -0400

    Third commit

--- bml_day4_a1/main.m
+++ bml_day4_a1/main.m
@@ -24,13 +24,43 @@ int main(int argc, const char * argv[]) {
         
         // Instantiating objects
         
+        // Creating a nissan object with a regular init
+        
         Car *nissan = [[Car alloc] init];
         NSLog(@"Created a %@", [nissan model]);
         
         
+        
+        // Creating a chevy object with a initWithModel init
+        
         Car *chevy = [[Car alloc] initWithModel:@"Chevy Corvette"];
         NSLog(@"Created a %@, too", chevy.model);
         
+        
+        
+        // Creating a delorean object
+        
+        Car *delorean = [[Car alloc] initWithModel:@"Delorean"];
+        
+        
+        // get the class of an object
+        
+        NSLog(@"%@ is an instance of the %@ class", [delorean model], [delorean class]);
+        
+        
+        // Check an object against a class and all subclasses
+        
+        if ([delorean isKindOfClass:[NSObject class]]) {
+            NSLog(@"%@ is an instance of NSObject or one of its subclasses", [delorean model]);
+        } else {
+            NSLog(@"%@ is not an instance of NSObject or one of its subclasses", [delorean model]);
+        }
+        
+        // Convert between strings and classes
+        
+        if (NSClassFromString(@"Car") == [Car class]) {
+            NSLog(@"I can convert between strings and classes!");
+        }
         
         
     
